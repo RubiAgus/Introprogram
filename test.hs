28b67b746f3484dc2e78418a76af1eee3a8c0f0e
Estudiante <agustin.n.rubinstein@gmail.com>
2024-04-17 19:31:16 -0300

    something's broken, fix at home, plz

--- test.hs
+++ test.hs
@@ -7,7 +7,7 @@ sumatoriaCompuesta n m
     | m == 1 = n
     | otherwise = n^m + sumatoriaCompuesta n (m-1) 
 
---Ex 16
+--Ex 16 a
 menorDivHasta :: Int -> Int -> Int 
 menorDivHasta n i 
     |  mod n i == 0 =  i
@@ -17,14 +17,33 @@ menorDiv:: Int -> Int
 menorDiv n 
     | n == 1 = 1
     | otherwise = menorDivHasta n  2
---esto llama a menorDivHasta y a i lo comienza en 2 y de ahì esto sigue tal que prueba todas las opciones hasta  llegar al que mod n i = o y te deberìa devolver i
 
---Ex 19, suma inicial de primos te dice si el numero dado puede formarse por una suma de los nùmeros primos en orden
+--16 b)
 esPrimo :: Int -> Bool
-esPrimo x = True 
-primosTest :: Int -> Bool
-primosTest x = True
+esPrimo x 
+    |menorDiv x == x = True
+    |otherwise = False 
+-- primosLista:: [Int] -> Int
+--     map. https://youtu.be/aTCeFgLRZbg?si=QBaXgxmxKg3PclnI Check that to see if it helps
+--Es primo debe usar  el menor divisor para que el menor divisor sea 1.
+--Ex 19, suma inicial de primos te dice si el numero dado puede formarse por una suma de los nùmeros primos en orden
+
      {-
 esto probablemente sea asociado  a ver si x es primo, luego de eso voy a querer empezar sumandole 1, luego 3, luego 5, 7 and so on and so forth.
-
+ARMÀ Una funcion que te sume una cantidad x de nùmeros primos en orden 
     -}
+--Requiero un contador de nùmeros primos, para ir sumandolos
+contadorPrimos:: Int -> Int
+contadorPrimos x    
+    | esPrimo x  == True
+
+-- sumaPrimos :: Int -> Int
+-- sumaPrimos x --Suma x cantidad de primos
+--     | x
+
+
+
+sSumaInicialDePrimos:: Int -> Bool
+sSumaInicialDePrimos n
+    | n = True
+    | otherwise = False
